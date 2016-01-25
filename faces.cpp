d2d161e304fe438576af6a67d8de5cf5bb7cdad2
David Keller <dnkeller@oakland.edu>
2016-01-25 15:22:52 -0500

    Added gitignore

--- faces.cpp
+++ faces.cpp
@@ -18,23 +18,23 @@ int main(int argc, const char *argv[]) {
   vector<Mat> images;
   vector<int> labels;
   // images for first person
-  images.push_back(imread("s1/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/4.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/5.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/6.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/7.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/8.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
-  images.push_back(imread("s1/9.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/4.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/5.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/6.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/7.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/8.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
+  images.push_back(imread("non-git/s1/9.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(0);
   // images for second person
-  images.push_back(imread("s11/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("s11/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("s11/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("non-git/s11/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("non-git/s11/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("non-git/s11/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
   
   model->train(images,labels);
 
-  Mat guess = imread("s3/10.pgm", CV_LOAD_IMAGE_GRAYSCALE);
+  Mat guess = imread("non-git/s11/10.pgm", CV_LOAD_IMAGE_GRAYSCALE);
   int label_guess = model->predict(guess);
 
   cout << label_guess << endl;
