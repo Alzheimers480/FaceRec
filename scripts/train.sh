2da2e8b2eb1ca33d530dd6c74874d29ef81be0a7
David Keller <dnkeller@oakland.edu>
2016-02-07 17:14:27 -0500

    More QA

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,4 @@
+~/cv-dev/create_csv.py ~/cv-dev/testdata/model > ~/cv-dev/faces.csv
 rm ~/cv-dev/outfiles/result.text
 ~/cv-dev/faces train ~/cv-dev/faces.csv
-time ~/cv-dev/scripts/recQA.sh
+time ~/cv-dev/scripts/recQA.sh -f "%e"
